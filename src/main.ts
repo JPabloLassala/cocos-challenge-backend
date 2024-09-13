83cc99e5f53a8aace360ed80f27966556a89a3b4
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-09-13 01:27:24 -0300

    CCC-3 Move App files to its own directory

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import { IAppConfig } from '@Config';
+import { AppModule } from '@App';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
