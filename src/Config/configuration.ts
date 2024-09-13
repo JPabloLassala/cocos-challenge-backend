995b70760d60a0f5cfbe958c6431e3468772073f
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-09-12 22:13:31 -0300

    CCC-2 Create initial config files

---
+++ src/Config/configuration.ts
@@ -0,0 +1,12 @@
+import { registerAs } from '@nestjs/config';
+
+export const appConfig = registerAs('app', () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  environment: process.env.NODE_ENV || 'development',
+  database: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+    username: process.env.DATABASE_USER || 'root',
+    password: process.env.DATABASE_PASSWORD || 'root',
+  },
+}));
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { appConfig } from '@Config';
+import { validate } from './Config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig],
+      validate: validate,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
