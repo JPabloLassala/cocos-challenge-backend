db9c825b8cbed7eba0a53d11b3468c56ad644da8
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-09-12 22:14:45 -0300

    CCC-2 Add path aliases

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { appConfig } from '@Config';
-import { validate } from './Config';
+import { appConfig, validate } from '@Config';
 
 @Module({
   imports: [
