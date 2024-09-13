e0fea82776303c2b904c6e8d058fa01b66a5aadb
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-09-12 22:28:58 -0300

    CCC-2 Create another interface for db config

--- src/Config/configuration.ts
+++ src/Config/configuration.ts
@@ -1,12 +1,26 @@
 import { registerAs } from '@nestjs/config';
 
+export interface IAppConfig {
+  port: number;
+  environment: string;
+}
+
+export interface IDatabaseConfig {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+}
+
 export const appConfig = registerAs('app', () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   environment: process.env.NODE_ENV || 'development',
-  database: {
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    username: process.env.DATABASE_USER || 'root',
-    password: process.env.DATABASE_PASSWORD || 'root',
-  },
+  database: {},
+}));
+
+export const databaseconfig = registerAs('database', () => ({
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+  username: process.env.DATABASE_USER || 'root',
+  password: process.env.DATABASE_PASSWORD || 'root',
 }));
