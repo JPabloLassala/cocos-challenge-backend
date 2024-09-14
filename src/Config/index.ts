fc433e1fac486d53568047d2f56eb1776ef79afa
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-09-13 21:28:42 -0300

    CCC-5 Add Exception filters

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -1,2 +1,3 @@
+export * from './config.constants';
 export * from './configuration';
 export * from './env.validation';
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import { IAppConfig } from '@Config';
 import { AppModule } from '@App';
+import { HttpExceptionFilter } from '@Utils';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const { port } = configService.get<IAppConfig>('app');
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(port);
   Logger.log(`Server running on http://localhost:${port}`, 'Bootstrap');
