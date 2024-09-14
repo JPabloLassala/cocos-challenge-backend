b722165a306cce55aa13155b44d5f2f80b646597
Juan Pablo Lassala <jpablolassala@gmail.com>
2024-09-13 21:29:39 -0300

    CCC-5 Add Utils classes

---
+++ src/Utils/all-exception.filter.ts
@@ -0,0 +1,38 @@
+import { Environments, IAppConfig } from '@Config';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { HttpAdapterHost } from '@nestjs/core';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(
+    private readonly httpAdapterHost: HttpAdapterHost,
+    private readonly configService: ConfigService,
+  ) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+    const ctx = host.switchToHttp();
+    const httpStatus = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    const { environment } = this.configService.get<IAppConfig>('app');
+    const getStack = () => {
+      if (environment !== Environments.Development) {
+        return undefined;
+      }
+      if (exception instanceof Error) {
+        return exception.stack.split('\n');
+      }
+      return exception;
+    };
+
+    const responseBody = {
+      statusCode: httpStatus,
+      message: exception instanceof Error ? exception.message : exception,
+      stack: getStack(),
+      timestamp: new Date().toISOString(),
+      path: httpAdapter.getRequestUrl(ctx.getRequest()),
+    };
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, httpStatus);
+  }
+}
